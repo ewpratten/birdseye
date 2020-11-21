d14938356da99784b687d77c5dd2ba2c01ac9bc9
Evan Pratten <ewpratten@gmail.com>
2020-11-21 10:53:02 -0500

    updates

---
+++ src/main/java/ca/retrylife/mc/birdseye/lib/DynmapConnection.java
@@ -0,0 +1,24 @@
+package ca.retrylife.mc.birdseye.lib;
+
+import java.net.URL;
+import java.util.function.Consumer;
+
+public class DynmapConnection {
+
+    public DynmapConnection(URL baseURL) {
+
+    }
+    
+    public void connect()  {
+
+    }
+    
+    public void setPlayerInformationCallback() {
+        
+    }
+
+    public void setServerTimeUpdateCallback(Consumer<Double> consumer){
+        
+    }
+    
+}
---
+++ src/main/java/ca/retrylife/mc/birdseye/lib/datastructures/local/WorldWeather.java
@@ -0,0 +1,16 @@
+package ca.retrylife.mc.birdseye.lib.datastructures.local;
+
+public class WorldWeather {
+
+    private boolean hasStorm;
+    private boolean isThundering;
+
+    public WorldWeather() {
+        this(false, false);
+    }
+
+    public WorldWeather(boolean hasStorm, boolean isThundering) {
+        
+    }
+    
+}
